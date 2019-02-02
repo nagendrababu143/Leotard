95ca7f586573d47d5a7e23df9e8b212f203860b9
Nagendra -PronixITS <nagendrababukolluri@gmail.com>
2019-02-02 10:56:42 +0530

    Initial commit

---
+++ app/src/main/java/android/pronixits/com/leotard/MainActivity.java
@@ -0,0 +1,13 @@
+package android.pronixits.com.leotard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
